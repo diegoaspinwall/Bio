b48a75ec1c91fdeb909c97ce0143e80211186375
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2017-12-17 18:47:30 -0500

    Updated from Brython Server: 12/17/2017 6:47:30 PM

--- SurfAVol.py
+++ SurfAVol.py
@@ -1,1 +1,10 @@
+#Diego Aspinwall
+#12-17-17
+#SurfAVol.py
 
+while True:
+    print('Enter "quit" to quit')
+    print('Enter "s" for Sphere')
+    print('Enter "c" for Cube')
+    print('Enter "t" for Tetrahedron')
+    print('Enter "s" for Icosahedron (20 sides)')
