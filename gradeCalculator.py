07349361fb0732456d210970d1e209de0bee7421
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2018-01-12 13:01:50 -0500

    Updated from Brython Server: 1/12/2018 1:01:50 PM

--- gradeCalculator.py
+++ gradeCalculator.py
@@ -1,1 +1,9 @@
+#Diego Aspinwall
+#1-12-18
+#gradeCalculator.py
+curgrade = int(input('Your current grade (%): '))
+want = int(input('You want at least (%): '))
+per = int(input('Your final is worth (%): '))
+print('You only need to score at least', str(blah)+'% on your final to get a', str(blah)+'% overall.')
 
+''' Grade = Exam Worth × Exam Score + (100% − Exam Worth) × Current Grade '''
