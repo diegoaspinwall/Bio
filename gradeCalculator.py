953c11eabe8169a07b2ba6e8c1a07125aebaf6b8
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2018-01-13 11:43:24 -0500

    Updated from Brython Server: 1/13/2018 11:43:24 AM

--- gradeCalculator.py
+++ gradeCalculator.py
@@ -5,4 +5,4 @@
 curgrade = int(input('Your current grade (%): '))
 want = int(input('You want at least (%): '))
 per = int(input('Your final is worth (%): '))
-print('You  need to score at least', str((-1/(per/100))*((-1)*want+(1-(per/100))*curgrade))+'% on your final to get a', str(want)+'% overall.')
+print('You  need to score at least', str(round((-1/(per/100))*((-1)*want+(1-(per/100))*curgrade),1))+'% on your final to get a', str(want)+'% overall.')
