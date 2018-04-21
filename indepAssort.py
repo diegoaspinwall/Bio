67f34f0fce65b0de93beb6de6a077a1ab58c50fe
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2018-04-21 18:29:37 -0400

    Updated from Brython Server: 4/21/2018 5:29:37 PM

--- indepAssort.py
+++ indepAssort.py
@@ -16,8 +16,12 @@ blueRectangle = RectangleAsset(10,20,blueOutline,blue)
 
 for i in range(0,23):
     print(i)
-    print(randint(0,1))
-    Sprite(blueRectangle, (0,20*i))
-    Sprite(redRectangle, (10,20*i))
+    num = randint(0,1)
+    if num==1:
+        Sprite(blueRectangle, (0,20*i))
+        Sprite(redRectangle, (10,20*i))
+    if num==0:
+        Sprite(blueRectangle, (10,20*i))
+        Sprite(redRectangle, (0,20*i))
 
 App().run()
