51cee77cd86fec252c34c2de65ce15b502d5deb3
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2018-04-21 18:33:26 -0400

    Updated from Brython Server: 4/21/2018 5:33:26 PM

--- indepAssort.py
+++ indepAssort.py
@@ -5,6 +5,8 @@
 from ggame import *
 from random import randint
 
+n = int(input('What is n? '))
+
 red = Color(0xFF0000,1)
 blue = Color(0x0000FF,1)
 black = Color(0x000000,1)
@@ -14,7 +16,7 @@ blackOutline = LineStyle(1,black)
 redRectangle = RectangleAsset(10,20,blackOutline,red)
 blueRectangle = RectangleAsset(10,20,blackOutline,blue)
 
-for i in range(0,23):
+for i in range(0,n):
     num = randint(0,1)
     if num==1:
         Sprite(blueRectangle, (0,20*i))
