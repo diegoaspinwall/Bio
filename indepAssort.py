976b8965402ec0eeeb95bbf666917d8ee6d74127
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2018-04-21 18:23:53 -0400

    Updated from Brython Server: 4/21/2018 5:23:52 PM

--- indepAssort.py
+++ indepAssort.py
@@ -1,1 +1,18 @@
+#Diego Aspinwall
+#4-21-18
+#indepAssort.py
 
+from ggame import *
+
+red = Color(0xFF0000,1)
+blue = Color(0x00FF00,1)
+
+blueOutline = LineStyle(1,blue)
+redOutline = LineStyle(1,red)
+
+redRectangle = RectangleAsset(200,75,redOutline,red)
+blueRectangle = RectangleAsset(200,75,blueOutline,blue)
+
+Sprite(blueRectangle)
+Sprite(redRectangle, (0,75))
+App().run()
