7833b077eb3c81127565d787d1cd1a6b11540995
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2018-04-21 18:31:46 -0400

    Updated from Brython Server: 4/21/2018 5:31:46 PM

--- indepAssort.py
+++ indepAssort.py
@@ -7,15 +7,14 @@ from random import randint
 
 red = Color(0xFF0000,1)
 blue = Color(0x0000FF,1)
+black = Color(0x000000,1)
 
-blueOutline = LineStyle(1,blue)
-redOutline = LineStyle(1,red)
+blackOutline = LineStyle(1,black)
 
-redRectangle = RectangleAsset(10,20,redOutline,red)
-blueRectangle = RectangleAsset(10,20,blueOutline,blue)
+redRectangle = RectangleAsset(10,20,blackOutline,red)
+blueRectangle = RectangleAsset(10,20,blackOutline,blue)
 
 for i in range(0,23):
-    print(i)
     num = randint(0,1)
     if num==1:
         Sprite(blueRectangle, (0,20*i))
