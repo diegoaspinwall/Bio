934966105e1091b683a6ab0e810c90fdb98ebfcf
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2018-05-06 17:41:03 -0400

    Updated from Brython Server: 5/6/2018 5:41:03 PM

--- cross.py
+++ cross.py
@@ -5,5 +5,5 @@
 print('This is a simulation that takes two flower parents, and calculates the probability of a color')
 print("The flower's color exhibits incomplete dominance")
 print('')
-male = int(input('What is the male genotype? 1=Heterozygous, 2= Homo red, 3= Homo white'))
-female = int(input('What is the female genotype? 1=Heterozygous, 2= Homo red, 3= Homo white'))
+male = int(input('What is the male genotype? 1=Heterozygous, 2= Homo red, 3= Homo white '))
+female = int(input('What is the female genotype? 1=Heterozygous, 2= Homo red, 3= Homo white '))
