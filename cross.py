5b287cf9f5849fc35afd036637b39268ebc8fdc6
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2018-05-06 17:44:55 -0400

    Updated from Brython Server: 5/6/2018 5:44:55 PM

--- cross.py
+++ cross.py
@@ -12,9 +12,10 @@ male = int(input('What is the male genotype? 1=Heterozygous, 2= Homo red, 3= Hom
 female = int(input('What is the female genotype? 1=Heterozygous, 2= Homo red, 3= Homo white '))
 
 red = Color(0xFF0000,1)
+pink = Color(0xFF80D5,1)
 white = Color(0xFFFFFF,1)
 black = Color(0x000000,1)
 
 blackOutline = LineStyle(1,black) 
 
-blueCircle = CircleAsset(50,blackOutline,blue)
+circle = CircleAsset(50,blackOutline,color)
