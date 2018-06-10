dfe9e863dd16794519fa3a9542ac22f391ffa9ba
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2018-06-10 18:05:23 -0400

    Updated from Brython Server: 6/10/2018 6:05:22 PM

--- flappy.py
+++ flappy.py
@@ -42,14 +42,6 @@ def moveBanana():
     banana.y = randint(0,ROWS-1)*CELL_SIZE
     data['frames'] = 0
 
-#updates score when monkey gets banana
-#updates text on screen
-def updateScore():
-    data['score'] += 10
-    data['scoreText'].destroy()
-    scoreBox = TextAsset('Score = '+str(data['score']))
-    data['scoreText'] = Sprite(scoreBox,(CELL_SIZE,(ROWS-3)*CELL_SIZE))
-
 #keeps track of how many frames have passed
 #moves banana if more than n have passed
 def step():
@@ -71,12 +63,10 @@ if __name__ == '__main__':
     jungleBox = RectangleAsset(COLS*CELL_SIZE,ROWS*CELL_SIZE, LineStyle(1,green),green)
     monkeyBox = RectangleAsset(CELL_SIZE,CELL_SIZE,LineStyle(1,brown), brown)
     bananaBox = RectangleAsset(CELL_SIZE,CELL_SIZE,LineStyle(1,yellow), yellow)
-    scoreBox = TextAsset('Score = 0')
     
     Sprite(jungleBox)
     banana = Sprite(bananaBox,((COLS*CELL_SIZE)/2,(ROWS*CELL_SIZE)/2))
     monkey = Sprite(monkeyBox)
-    data['scoreText'] = Sprite(scoreBox,(CELL_SIZE,(ROWS-3)*CELL_SIZE))
     
     App().listenKeyEvent('keydown','right arrow',moveRight)
     App().listenKeyEvent('keydown','left arrow',moveLeft)
