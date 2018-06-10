b855598a8e2cc8878ca6e5f3dfb737a1442c64c2
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2018-06-10 18:29:22 -0400

    Updated from Brython Server: 6/10/2018 6:29:21 PM

--- flappy.py
+++ flappy.py
@@ -14,7 +14,7 @@ CELL_SIZE = 20
 def moveUp(event):
     if monkey.y > 0:
         #monkey.y -= CELL_SIZE
-        data['drop'] = -3
+        data['drop'] = -5
 
 #keeps track of how many frames have passed
 def step():
