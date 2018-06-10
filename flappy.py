0335d95886f88aa6047beab94ac05f56781cb435
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2018-06-10 18:04:09 -0400

    Updated from Brython Server: 6/10/2018 6:04:08 PM

--- flappy.py
+++ flappy.py
@@ -1,1 +1,85 @@
+#Diego Aspinwall
+#6-10-18
+#flappy.py
 
+from ggame import *
+from random import *
+
+#constants
+ROWS = 30
+COLS = 40
+CELL_SIZE = 20
+
+#moves monkey right/l/u/d
+def moveRight(event):
+    if monkey.x < (COLS-1)*CELL_SIZE:
+        monkey.x += CELL_SIZE
+        if monkey.x == banana.x and monkey.y == banana.y:
+            moveBanana()
+            updateScore()
+def moveLeft(event):
+    if monkey.x > 0:
+        monkey.x -= CELL_SIZE
+        if monkey.x == banana.x and monkey.y == banana.y:
+            moveBanana()
+            updateScore()
+def moveUp(event):
+    if monkey.y > 0:
+        monkey.y -= CELL_SIZE
+        if monkey.x == banana.x and monkey.y == banana.y:
+            moveBanana()
+            updateScore()
+def moveDown(event):
+    if monkey.y < (ROWS-1)*CELL_SIZE:
+        monkey.y += CELL_SIZE
+        if monkey.x == banana.x and monkey.y == banana.y:
+            moveBanana()
+            updateScore()
+
+#moves banana to random location
+def moveBanana():
+    banana.x = randint(0,COLS-1)*CELL_SIZE
+    banana.y = randint(0,ROWS-1)*CELL_SIZE
+    data['frames'] = 0
+
+#updates score when monkey gets banana
+#updates text on screen
+def updateScore():
+    data['score'] += 10
+    data['scoreText'].destroy()
+    scoreBox = TextAsset('Score = '+str(data['score']))
+    data['scoreText'] = Sprite(scoreBox,(CELL_SIZE,(ROWS-3)*CELL_SIZE))
+
+#keeps track of how many frames have passed
+#moves banana if more than n have passed
+def step():
+    data['frames'] += 1
+    if data['frames'] == 200:
+        moveBanana()
+
+#runs the game
+if __name__ == '__main__':
+    
+    data = {}
+    data['score'] = 0
+    data['frames'] = 0
+    
+    green = Color(0x006600,1)
+    brown = Color(0x8B4513,1)
+    yellow = Color(0xFFFF00,1)
+    
+    jungleBox = RectangleAsset(COLS*CELL_SIZE,ROWS*CELL_SIZE, LineStyle(1,green),green)
+    monkeyBox = RectangleAsset(CELL_SIZE,CELL_SIZE,LineStyle(1,brown), brown)
+    bananaBox = RectangleAsset(CELL_SIZE,CELL_SIZE,LineStyle(1,yellow), yellow)
+    scoreBox = TextAsset('Score = 0')
+    
+    Sprite(jungleBox)
+    banana = Sprite(bananaBox,((COLS*CELL_SIZE)/2,(ROWS*CELL_SIZE)/2))
+    monkey = Sprite(monkeyBox)
+    data['scoreText'] = Sprite(scoreBox,(CELL_SIZE,(ROWS-3)*CELL_SIZE))
+    
+    App().listenKeyEvent('keydown','right arrow',moveRight)
+    App().listenKeyEvent('keydown','left arrow',moveLeft)
+    App().listenKeyEvent('keydown','up arrow',moveUp)
+    App().listenKeyEvent('keydown','down arrow',moveDown)
+    App().run(step)
