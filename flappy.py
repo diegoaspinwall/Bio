3cd9d2590933df4d06c46c9d2069e4b3e29b167e
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2018-06-10 18:08:36 -0400

    Updated from Brython Server: 6/10/2018 6:08:35 PM

--- flappy.py
+++ flappy.py
@@ -30,18 +30,12 @@ if __name__ == '__main__':
     
     blue = Color(0x000066,1)
     brown = Color(0x8B4513,1)
-    yellow = Color(0xFFFF00,1)
     
     jungleBox = RectangleAsset(COLS*CELL_SIZE,ROWS*CELL_SIZE, LineStyle(1,blue),blue)
     monkeyBox = RectangleAsset(CELL_SIZE,CELL_SIZE,LineStyle(1,brown), brown)
-    bananaBox = RectangleAsset(CELL_SIZE,CELL_SIZE,LineStyle(1,yellow), yellow)
     
     Sprite(jungleBox)
-    banana = Sprite(bananaBox,((COLS*CELL_SIZE)/2,(ROWS*CELL_SIZE)/2))
     monkey = Sprite(monkeyBox)
-    
-    App().listenKeyEvent('keydown','right arrow',moveRight)
-    App().listenKeyEvent('keydown','left arrow',moveLeft)
+
     App().listenKeyEvent('keydown','up arrow',moveUp)
-    App().listenKeyEvent('keydown','down arrow',moveDown)
     App().run(step)
